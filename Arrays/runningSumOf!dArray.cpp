71eead8c09f96904fc6d8000efbde21a3e5c7a89
DRISHTI-SINGH <drishtisingh1210@gmail.com>
2022-06-01 23:57:43 +0530

    Running sum of 1d array

--- Arrays/runningSumOf!dArray.cpp
+++ Arrays/runningSumOf!dArray.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1)
-            return nums;
-        int j=0;
-        for(int i=1;i<n;i++){
-            if(i==n-1){
-                int u = nums[i]+nums[j];
-                nums[i]=u;
-                return nums;
-            }
-            int update = nums[j]+nums[i];
-            nums[i]=update;
-            j++;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(i > 0) nums[i] += nums[i-1];
         }
         return nums;
     }
