80129354ddc6b8df28f9e78c60774023a8882bf5
DRISHTI-SINGH <drishtisingh1210@gmail.com>
2022-06-20 22:58:31 +0530

    Trie

---
+++ Trie/PrefixSuffix.cpp
@@ -0,0 +1,67 @@
+// Design a special dictionary that searches the words in it by a prefix and a suffix.
+
+// Implement the WordFilter class:
+
+// WordFilter(string[] words) Initializes the object with the words in the dictionary.
+// f(string prefix, string suffix) Returns the index of the word in the dictionary, which has the prefix prefix and the suffix suffix. If there is more than one valid index, return the largest of them. If there is no such word in the dictionary, return -1.
+ 
+
+// Example 1:
+
+// Input
+// ["WordFilter", "f"]
+// [[["apple"]], ["a", "e"]]
+// Output
+// [null, 0]
+
+// Explanation
+// WordFilter wordFilter = new WordFilter(["apple"]);
+// wordFilter.f("a", "e"); // return 0, because the word at index 0 has prefix = "a" and suffix = 'e".
+ 
+
+// Constraints:
+
+// 1 <= words.length <= 15000
+// 1 <= words[i].length <= 10
+// 1 <= prefix.length, suffix.length <= 10
+// words[i], prefix and suffix consist of lower-case English letters only.
+// At most 15000 calls will be made to the function f.
+
+
+class WordFilter {
+    TrieNode trie;
+    public WordFilter(String[] words) {
+        trie = new TrieNode();
+        for (int weight = 0; weight < words.length; ++weight) {
+            String word = words[weight] + "{";
+            for (int i = 0; i < word.length(); ++i) {
+                TrieNode cur = trie;
+                cur.weight = weight;
+                for (int j = i; j < 2 * word.length() - 1; ++j) {
+                    int k = word.charAt(j % word.length()) - 'a';
+                    if (cur.children[k] == null)
+                        cur.children[k] = new TrieNode();
+                    cur = cur.children[k];
+                    cur.weight = weight;
+                }
+            }
+        }
+    }
+    public int f(String prefix, String suffix) {
+        TrieNode cur = trie;
+        for (char letter: (suffix + '{' + prefix).toCharArray()) {
+            if (cur.children[letter - 'a'] == null) return -1;
+            cur = cur.children[letter - 'a'];
+        }
+        return cur.weight;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    int weight;
+    public TrieNode() {
+        children = new TrieNode[27];
+        weight = 0;
+    }
+}
