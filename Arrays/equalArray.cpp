61fd0db521d77ef3d1225137c5aecba9b391851e
DRISHTI-SINGH <drishtisingh1210@gmail.com>
2022-06-30 23:18:04 +0530

    Array

--- Arrays/equalArray.cpp
+++ Arrays/equalArray.cpp
@@ -1,3 +1,32 @@
+//#################### Minimum Moves to Equal Array Elements II #################
+//-------------------------------------------------------------------------------
+// Given an integer array nums of size n, return the minimum number of moves required to make all array elements equal.
+
+// In one move, you can increment or decrement an element of the array by 1.
+
+// Test cases are designed so that the answer will fit in a 32-bit integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 2
+// Explanation:
+// Only two moves are needed (remember each move increments or decrements one element):
+// [1,2,3]  =>  [2,2,3]  =>  [2,2,2]
+// Example 2:
+
+// Input: nums = [1,10,2,9]
+// Output: 16
+ 
+
+// Constraints:
+
+// n == nums.length
+// 1 <= nums.length <= 105
+
+
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
@@ -6,6 +35,20 @@ public:
         int median = nums[n/2]; // Finding median
         for(int i=0; i<n; i++){
             steps += abs(nums[i] - median); //Adding absolute difference
+        }
+        return steps;
+    }
+};
+
+// Random Access Method
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n = nums.size(), steps = 0;
+        nth_element(nums.begin(), nums.begin()+(n/2), nums.end()); //Fixing ths median element
+        int median = nums[n/2];
+        for(int i=0; i<n; i++){
+            steps += abs(nums[i] - median);
         }
         return steps;
     }
