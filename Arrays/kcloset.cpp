a83e1d596e43e8af74484ab9b099ea6a4268b539
DRISHTI-SINGH <drishtisingh1210@gmail.com>
2022-09-30 23:50:41 +0530

    Array

--- Arrays/kcloset.cpp
+++ Arrays/kcloset.cpp
@@ -52,3 +52,17 @@ vector<int> findClosestElements(vector<int>& A, int k, int x) {
         }
         return vector<int>(A.begin() + left, A.begin() + left + k);
     }
+
+
+//another one
+    class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {        
+        int n = size(arr), R = lower_bound(begin(arr), end(arr), x) - begin(arr), L = R - 1;
+		// expand the [L, R] window till its size becomes equal to k
+        while(k--) 
+            if(R >= n || L >= 0 && x - arr[L] <= arr[R] - x) L--;  // expand from left
+            else R++;                                              // expand from right
+        return vector<int>(begin(arr) + L + 1, begin(arr) + R);
+    }
+};
